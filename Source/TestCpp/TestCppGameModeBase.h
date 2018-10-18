6f4d040af145c671d9075b95ef8b99550e6d2cdb
y <y>
2018-10-18 10:08:34 +0900

    up

---
+++ Source/TestCpp/TestCppGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTCPP_API ATestCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/TestCpp/UserWidgetBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UserWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTCPP_API UUserWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
