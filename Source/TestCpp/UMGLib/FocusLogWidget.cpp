a0d1ea974de38f6e3105cfd9e7dbf686eb478259
y <y>
2018-10-30 15:48:17 +0900

    focus log fix

--- Source/TestCpp/UMGLib/FocusLogWidget.cpp
+++ Source/TestCpp/UMGLib/FocusLogWidget.cpp
@@ -2,15 +2,60 @@
 
 #include "FocusLogWidget.h"
 
-void UFocusLogWidget::CreateItemLogMessage()
+void UFocusLogWidgetItem::OnFinishAnimation()
+{
+	OnEventDispather_FinishAnime.Broadcast(id);
+	RemoveFromParent();
+}
+
+void UFocusLogWidget::CreateItemLogMessage(UFocusLogWidgetItem* createItemWidget, FString inMessage)
 {
 	if (VerticalBoxItem == nullptr) return;
+	if (m_FocusList.Num() >= 6){RemoveIdMin();} // ŒÃ‚¢‚à‚Ì‚ğíœ
+	VerticalBoxItem->AddChildToVerticalBox(createItemWidget);
+	m_FocusList.Add(createItemWidget);
+	index++;
+	createItemWidget->SetRenderScale(FVector2D(1.0f, -1.0f));	
+	createItemWidget->id = index;
+}
 
 
-	UWidget* tmpwidget = nullptr;
 
+bool UFocusLogWidget::RemoveFocusItemList(int32 idx)
+{
+	bool retval = false;
+	if (m_FocusList.Num() == 0) return false; // ‘¶İ‚µ‚È‚¢
 
-	//CreateWidget<UFocusLogWidgetItem>()
+	UFocusLogWidgetItem* romovewidget = nullptr;
+	for (int32 i = 0; i < m_FocusList.Num(); ++i)
+	{
+		if (m_FocusList[i]->id == idx)
+		{
+			romovewidget = m_FocusList[i];
+			break;
+		}
+	}
+	if (romovewidget != nullptr) {
+		m_FocusList.Remove(romovewidget);
+		romovewidget->RemoveFromParent(); // ‚±‚±‚Å‚â‚é
 
-	VerticalBoxItem->AddChildToVerticalBox(tmpwidget);
+	}
+	return retval;
+}
+
+void UFocusLogWidget::RemoveIdMin()
+{
+	if (index == 0 || m_FocusList.Num() == 0) return; // ‘¶İ‚µ‚È‚¢
+
+	// ŒÃ‚¢‚à‚Ì‚ğíœ‚·‚é
+	// ˆê”ÔID‚ª¬‚³‚¢‚à‚Ì
+	int32 minid = index;
+	for (int32 ji = 0; ji < m_FocusList.Num(); ji++)
+	{
+		if (m_FocusList[ji]->id < minid)
+		{
+			minid = m_FocusList[ji]->id;
+		}
+	}
+	RemoveFocusItemList(index);
 }
--- Source/TestCpp/UMGLib/FocusLogWidget.h
+++ Source/TestCpp/UMGLib/FocusLogWidget.h
@@ -9,7 +9,10 @@
 #include "FocusLogWidget.generated.h"
 
 
-/**
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FocusLogWidgetItemEventDispather, int32, inIdx);
+
+// ----------------------------------------------------------
+/** 
  *
  */
 UCLASS()
@@ -17,9 +20,20 @@ class TESTCPP_API UFocusLogWidgetItem : public UUserWidget
 {
 	GENERATED_BODY()
 
+public:
+	UFUNCTION(BlueprintCallable, Category = "FocusLogWidgetItem")
+	void OnFinishAnimation();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,  Category = "FocusLogWidgetItem")
+	int32 id;
+
+	UPROPERTY(BlueprintAssignable, BlueprintReadWrite,  Category = "FocusLogWidgetItem")
+	FocusLogWidgetItemEventDispather OnEventDispather_FinishAnime;
 };
 
 
+
+// ----------------------------------------------------------
 /**
  * 
  */
@@ -27,20 +41,34 @@ UCLASS()
 class TESTCPP_API UFocusLogWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
 public:
 	UFUNCTION(BlueprintCallable, Category = "FocusLogWidget")
 	/// <summary>
 	/// メッセージItem追加
 	/// </summary>
-	void CreateItemLogMessage();
-
+	void CreateItemLogMessage(UFocusLogWidgetItem* createItemWidget, FString inMessage);
 
 	UFUNCTION(BlueprintCallable, Category = "FocusLogWidget")
 	void SetVerticalBox(UVerticalBox* inVerticalBoxItem) {
 		VerticalBoxItem = inVerticalBoxItem;
 	}
 
+	UFUNCTION(BlueprintCallable, Category = "FocusLogWidget")
+	bool RemoveFocusItemList(int32 idx);
 
+protected:
+	void RemoveIdMin();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FocusLogWidget")
 	UVerticalBox* VerticalBoxItem;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FocusLogWidget")
+	TArray<UFocusLogWidgetItem*> m_FocusList;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FocusLogWidget")
+	int32 index;
+
 };
+
+// ----------------------------------------------------------
